c77e2cd1b689dcb7533577283d57f9fe2d6c4933
Ondřej Benkovský <ondrej.benkovsky@jamf.com>
2022-10-29 21:07:05 +0200

    release 2.4.0

--- Formula/dnspyre.rb
+++ Formula/dnspyre.rb
@@ -2,15 +2,15 @@ class Dnspyre < Formula
   desc "CLI tool for a high QPS DNS benchmark"
   homepage "https://github.com/Tantalor93/dnspyre"
   url "git@github.com:Tantalor93/dnspyre", :using => :git,
-      revision: "f72b4d843ff525a8eec5e84cb402a058427adad3"
-  version "2.3.1"
+      revision: "b76130dee0afd8c216e8e54b8c1ecad80708191e"
+  version "2.4.0"
 
   head "git@github.com:Tantalor93/dnspyre.git", :using => :git
 
   depends_on "go" => :build
 
   def install
-    system "make", "VERSION=2.3.1", "build"
+    system "make", "VERSION=2.4.0", "build"
     bin.install "bin/dnspyre"
   end
 end
